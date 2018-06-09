ef4471028863ccc04893fec6ba5fce7000d01557
hatelove <jay.hatelove@gmail.com>
2018-06-09 11:41:53 +0800

    refactor: introduce scoreLookup

--- TennisScore/TennisGame.cs
+++ TennisScore/TennisGame.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TennisScore
 {
     public class TennisGame
     {
         private readonly IRepository<Game> _repo;
+
+        private Dictionary<int, string> _scoreLookup = new Dictionary<int, string>
+        {
+            {1, "Fifteen"},
+            {2, "Thirty"},
+        };
 
         public TennisGame(IRepository<Game> repo)
         {
@@ -15,15 +22,11 @@ namespace TennisScore
         {
             var game = this._repo.GetGame(gameId);
 
-            if (game.FirstPlayerScore == 2)
+            if (game.FirstPlayerScore > 0)
             {
-                return "Thirty Love";
+                return _scoreLookup[game.FirstPlayerScore] + " Love";
             }
 
-            if (game.FirstPlayerScore == 1)
-            {
-                return "Fifteen Love";
-            }
 
             return "Love All";
         }
